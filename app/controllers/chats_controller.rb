ee953779cb826b4f9aaa51a9bd02f0737d66c47a
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-05-12 21:06:52 +0900

    [edit]chats_controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,11 @@
 class ChatsController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    rooms = current_user.user_rooms.pluck(:room_id)
+    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+
     
+  end
 end
 
     
